64737a17659fbae5075f5982c890f20e5c356a1d
Yo Higashida <y.higashida0226+app@gmail.com>
2022-06-30 13:11:50 +0900

    add: present

--- PhotoApp/Controller/WelcomeViewController.swift
+++ PhotoApp/Controller/WelcomeViewController.swift
@@ -13,17 +13,10 @@ class WelcomeViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // TODO: 一時的にTopViewに遷移するように変更
+        let nextVC = storyboard?.instantiateViewController(withIdentifier: "CommunityTopVC") as! CommunityTopViewController
+        let nav = UINavigationController(rootViewController: nextVC)
+        nav.modalPresentationStyle = .fullScreen
+        self.present(nav, animated: true)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
