b994b4a6c8a1f266600780af9d06b92c9825b86b
Yo Higashida <y.higashida0226+app@gmail.com>
2022-06-30 13:23:33 +0900

    add: edit-member

--- PhotoApp/Controller/CommunityTopViewController.swift
+++ PhotoApp/Controller/CommunityTopViewController.swift
@@ -13,17 +13,12 @@ class CommunityTopViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        title = "Sample Community"
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func tappedAddButton() {
+        let nextVC = storyboard?.instantiateViewController(withIdentifier: "EditMemberVC") as! EditMemberViewController
+        let navigationController = UINavigationController(rootViewController: nextVC)
+        present(navigationController, animated: true)
     }
-    */
-
 }
---
+++ PhotoApp/Controller/EditMemberViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddMemberViewController.swift
+//  PhotoApp
+//
+//  Created by Yo Higashida on 2022/06/30.
+//
+
+import UIKit
+
+class EditMemberViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        navigationController?.isToolbarHidden = false
+        title = "Edit Member"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
